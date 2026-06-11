4a450b9fc921e4bc72bcb604e16d2d78277a876c
Achaz von Hardenberg <achaz.hardenberg@unipv.it>
2026-06-11 13:18:45 +0200

    Feature: add toggleable update notification for NumPyro backend to .onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -42,3 +42,18 @@ if (getRversion() >= "2.15.1") {
 # Internal environment for storing dynamic functions (e.g. for NIMBLE)
 # to avoid polluting the global environment and satisfy CRAN requirements.
 .because_env <- new.env(parent = emptyenv())
+
+.onAttach <- function(libname, pkgname) {
+  # Toggle this to TRUE whenever you release a major update to the Python
+  # backend and want to notify your R users to upgrade.
+  notify_numpyro_update <- FALSE 
+  
+  if (notify_numpyro_update) {
+    packageStartupMessage(
+      "----------------------------------------------------------------------\n",
+      " NOTE: A new version of the NumPyro backend (because_py) is available!\n",
+      " Run `install_because_numpyro()` to upgrade and get the latest features.\n",
+      "----------------------------------------------------------------------"
+    )
+  }
+}
